5157b870669d7a55d52b6c1ebb30a8adaeb6af44
jhonboy121 <alfredmathew05@gmail.com>
2022-03-26 19:38:28 +0530

    quickstep: RecentsView: add null check for invocations mFirstFloatingTaskView
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Iae0fb3b14b2ef4a48f906e0f8524fddaebe1b850

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -4216,7 +4216,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                 mActivity.getDeviceProfile(),
                 mSplitSelectStateController.getActiveSplitStagePosition(), firstTaskEndingBounds,
                 secondTaskEndingBounds);
-
+        if (mFirstFloatingTaskView == null) return false;
         mFirstFloatingTaskView.getBoundsOnScreen(firstTaskStartingBounds);
         mFirstFloatingTaskView.addConfirmAnimation(pendingAnimation,
                 new RectF(firstTaskStartingBounds), firstTaskEndingBounds,
@@ -4320,8 +4320,10 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                 mSplitPlaceholderInset, mActivity.getDeviceProfile(),
                 mSplitSelectStateController.getActiveSplitStagePosition(), mTempRect);
         mTempRectF.set(mTempRect);
-        mFirstFloatingTaskView.updateOrientationHandler(mOrientationHandler);
-        mFirstFloatingTaskView.update(mTempRectF, /*progress=*/1f);
+	if (mFirstFloatingTaskView != null) {
+        	mFirstFloatingTaskView.updateOrientationHandler(mOrientationHandler);
+        	mFirstFloatingTaskView.update(mTempRectF, /*progress=*/1f);
+	}
 
         PagedOrientationHandler orientationHandler = getPagedOrientationHandler();
         Pair<FloatProperty, FloatProperty> taskViewsFloat =
