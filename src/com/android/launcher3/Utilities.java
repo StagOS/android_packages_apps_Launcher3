ad04ee3a09159f8fd99cfcbe8e40291acdf5f3a0
Nauval Rizky <enuma.alrizky@gmail.com>
2022-07-03 01:01:53 +0000

    Launcher3: Add option to control wallpaper zooming
    @neobuddy89: Updated for A13 QPR1
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -173,6 +173,8 @@ public final class Utilities {
     public static final String LENS_SHARE_ACTIVITY = "com.google.android.apps.search.lens.LensShareEntryPointActivity";
     public static final String KEY_SMARTSPACE = "pref_smartspace";
 
+    public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
+
     /**
      * Returns true if theme is dark.
      */
@@ -1020,4 +1022,9 @@ public final class Utilities {
         return prefs.getBoolean(KEY_SMARTSPACE, true);
     }
 
+
+    public static boolean canZoomWallpaper(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOMING, true);
+    }
 }
