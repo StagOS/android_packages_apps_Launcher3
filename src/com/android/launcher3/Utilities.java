2428a9bcb9b3df2018bc0c6e1ffe9311dcca29c5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-01 00:33:24 +0530

    Launcher3: Move google settings to new category
    Make it dependent on GSA/ASI and reload launcher when either
    of them are disabled/enabled.
    
    Change-Id: I14c09463f7df115ffe12021370b0b1008177430c

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -167,6 +167,7 @@ public final class Utilities {
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+    public static final String ASI_PACKAGE = "com.google.android.as";
     public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
     public static final String LENS_URI = "google://lens";
     public static final String LENS_SHARE_ACTIVITY = "com.google.android.apps.search.lens.LensShareEntryPointActivity";
@@ -983,6 +984,14 @@ public final class Utilities {
         }
     }
 
+    public static boolean isASIEnabled(Context context) {
+        try {
+            return context.getPackageManager().getApplicationInfo(ASI_PACKAGE, 0).enabled;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static boolean isDoubleTapGestureEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DT_GESTURE, true);
@@ -1003,7 +1012,7 @@ public final class Utilities {
     }
 
     public static boolean showSmartspace(Context context) {
-        return isGSAEnabled(context) && isSmartspaceEnabled(context);
+        return isGSAEnabled(context) && isASIEnabled(context) && isSmartspaceEnabled(context);
     }
 
     private static boolean isSmartspaceEnabled(Context context) {
